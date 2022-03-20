48aa51f7262935f2889d27bb1dd63678dd4752f9
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2022-03-20 22:36:21 +0100

    format

--- app.py
+++ app.py
@@ -18,18 +18,19 @@ LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 f = open("__id", "r")
-rpi_id = f.read() 
+rpi_id = f.read()
 
 if not devMode:
-    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ,
+                       LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     strip.begin()
 
-    color = Color(0, 0, 0)
 
 def set_color(strip, color):
     for i in range(strip.numPixels()):
@@ -39,29 +40,29 @@ def set_color(strip, color):
 
 async def start(uri):
     async with websockets.connect(uri) as websocket:
-      await websocket.send("new device," + rpi_id)
-
-      while True:
-        try:
-            msg = await websocket.recv()
-        except websockets.ConnectionClosed:
-            print(f"Terminated")
-            break
-
-        if (msg == "start"):
-            print("shining")
-            if not devMode:
-                color = Color(255, 255, 255)
-
-        elif msg == "stop":
-            print("stopping")
-            if not devMode:
-                color = Color(0, 0, 0)
-
         if not devMode:
-            set_color(strip, color)
+            color = Color(0, 0, 0)
+        await websocket.send("new device," + rpi_id)
+
+        while True:
+            try:
+                msg = await websocket.recv()
+            except websockets.ConnectionClosed:
+                print(f"Terminated")
+                break
+
+            if (msg == "start"):
+                print("shining")
+                if not devMode:
+                    color = Color(255, 255, 255)
+
+            elif msg == "stop":
+                print("stopping")
+                if not devMode:
+                    color = Color(0, 0, 0)
+
+            if not devMode:
+                set_color(strip, color)
 
 
 asyncio.run(start("ws://192.168.137.1:8080"))
-
-
