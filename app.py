7d5c6edde8e94ff2f6338326496275cc51d2ba00
dorianzgraggen <39463388+dorianzgraggen@users.noreply.github.com>
2022-03-20 22:16:30 +0100

    try turning lights on and off

--- app.py
+++ app.py
@@ -1,9 +1,45 @@
-import asyncio
-from websockets import connect
+# based of https://github.com/rpi-ws281x/rpi-ws281x-python/blob/master/examples/strandtest.py
 
-async def hello(uri):
-    async with connect(uri) as websocket:
-      await websocket.send("here i am")
+import asyncio
+import websockets
+
+devMode = False
+
+try:
+    from rpi_ws281x import PixelStrip, Color
+except ImportError:
+    print("Warning: Could not import 'rpi_ws281x', assuming you're in dev mode")
+    devMode = True
+
+# LED strip configuration:
+LED_COUNT = 32        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+f = open("__id", "r")
+rpi_id = f.read() 
+
+if not devMode:
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    color = Color(0, 0, 0)
+
+def set_color(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+
+
+async def start(uri):
+    async with websockets.connect(uri) as websocket:
+      await websocket.send("new device," + rpi_id)
 
       while True:
         try:
@@ -12,6 +48,20 @@ async def hello(uri):
             print(f"Terminated")
             break
 
-        print(msg)
+        if (msg == "start"):
+            print("shining")
+            if not devMode:
+                color = Color(255, 255, 255)
 
-asyncio.run(hello("ws://localhost:8080"))
+        elif msg == "stop":
+            print("stopping")
+            if not devMode:
+                color = Color(0, 0, 0)
+
+        if not devMode:
+            set_color(strip, color)
+
+
+asyncio.run(start("ws://192.168.137.1:8080"))
+
+
